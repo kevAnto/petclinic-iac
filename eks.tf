01edcb20348aedfd9669111b6720d37a71d86da8
Antoine CHOULA <76167680+kevAnto@users.noreply.github.com>
2024-05-31 13:53:59 +0100

    init

--- eks.tf
+++ eks.tf
@@ -4,7 +4,7 @@ locals {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5"
+  version = "20.12.0"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.29"
--- provider.tf
+++ provider.tf
@@ -29,7 +29,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.8.1"
+      version = "5.40"
     }
   }
 
